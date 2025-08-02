67dedd8b1ed30b5e6fc653ee542e48a2e29d099e
Mark Haslinghuis <mark@numloq.nl>
2025-08-02 23:40:02 +0200

    Update build options (#14565)
    (cherry picked from commit 3f79c25bc7884922a32137b23a9075fcd978ffe3)

--- src/main/msp/msp_build_info.h
+++ src/main/msp/msp_build_info.h
@@ -57,6 +57,7 @@
 #define BUILD_OPTION_AKK_SMARTAUDIO             16405
 #define BUILD_OPTION_BATTERY_CONTINUE           16406
 #define BUILD_OPTION_CAMERA_CONTROL             16407
+#define BUILD_OPTION_CHIRP                      16426
 #define BUILD_OPTION_DASHBOARD                  16408
 #define BUILD_OPTION_EMFAT_TOOLS                16409
 #define BUILD_OPTION_ESCSERIAL_SIMONK           16410
